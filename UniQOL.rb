d663d41086fb7d04f52305591c9300867c72f488
birblett <bwee.sylv@gmail.com>
2025-05-09 22:38:27 -0700

    fix various bad injection targets

--- UniQOL.rb
+++ UniQOL.rb
@@ -350,10 +350,20 @@ end unless UniLib.mod_included?("Learn_Egg_moves")
 #=========================================================== RELEARN_PREEVO_MOVES =========================================================#
 #================================================================ AMB PORT ================================================================#
 
-if ENABLE_PREEVO_RELEARN_OPTION
+if ENABLE_PREEVO_RELEARN_OPTION and Rejuv
 
   PREVO_RELEARN_OPTION = UniStringOption.new("PreEvo Relearn", "Learn moves from pre-evolutions.", %w[Off On])
 
+  UniLib.insert_in_function(:pbEachNaturalMove, :TAIL,
+    "if PREVO_RELEARN_OPTION == 1
+      prevo, cache = pbGetPreviousForm(pokemon.species,pokemon.form), $cache.pkmn
+      until prevo[0].nil? or prevo[1].nil? or %w[Mega Primal].include?(name = cache[prevo[0]].forms[prevo[1]])
+        ((prevo[1] == 0 || (cache[prevo[0]].formData.dig(name,:Moveset).nil? && (prevo[1] = 0) == 0)) ?
+           cache[prevo[0]].Moveset : cache[prevo[0]].formData.dig(name,:Moveset)).each { |mv| yield mv[1], mv[0] }
+        break if prevo == (tmp = pbGetPreviousForm(prevo[0],prevo[1]))
+        prevo = tmp
+      end
+    end")
   UniLib.insert_in_function(:pbEachNaturalMove, :TAIL,
     "if PREVO_RELEARN_OPTION == 1
       prevo, cache = pbGetPreviousForm(pokemon.species,pokemon.form), $cache.pkmn
@@ -482,7 +492,7 @@ unless UniLib.mod_included?("FixEggMoves")
   UniLib.replace_in_method(:PokeBattle_Pokemon, :getEggMoveList, "movelist = $cache.pkmn[babyspecies[0]].formData.dig(formname,:EggMoves)",
     "movelist = $cache.pkmn[babyspecies[0]].formData.dig(formname, :EggMoves) unless $cache.pkmn[babyspecies[0]].formData.dig(formname, :EggMoves).nil?")
 
-end
+end if Rejuv
 
 #==========================================================================================================================================#
 #================================================================ FEATURES ================================================================#
@@ -792,7 +802,7 @@ if ENABLE_UNREAL_CLOCK
     "unless $Settings.unrealTimeDiverge == 0
       @cmdUnrealClock = -1
       @buttons[@cmdUnrealClock = @buttons.length] = \"Unreal Clock\"
-    end")
+    end") if Rejuv
 
   target = Reborn ? :TAIL : "if ($game_switches[:NotPlayerCharacter] == false ||  $game_switches[:InterceptorsWish] == true)"
   UniLib.insert_in_method_before(:Scene_Pokegear, :checkChoice, target,
