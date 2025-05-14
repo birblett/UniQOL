7d97a2814a4d03678f1347cc04dbfa254eeca332
birblett <bwee.sylv@gmail.com>
2025-05-13 18:37:52 -0700

    restructure to conform to new modloader spec

--- UniQOL.rb
+++ UniQOL.rb
@@ -1,9 +1,3 @@
-if Reborn
-  require "patch/Mods/UniLib/StandardAPI"
-else
-  require "Data/Mods/UniLib/StandardAPI"
-end
-
 UniLib.verify_version(0.6, __FILE__)
 UniLib.include "Options"
 UniLib.include "Asset"
@@ -50,9 +44,9 @@ ENABLE_QUICK_ACCESS = true
 
 NESTED = true
 
-def uniqol_asset_relative(path) = "#{NESTED ? "UniQOL/" : ""}UniQOLAssets/#{path}"
+def uniqol_asset_relative(path) = "UniQOL/UniQOLAssets/#{path}"
 
-def uniqol_asset(path) = UniLib.path "#{NESTED ? "UniQOL/" : ""}UniQOLAssets/#{path}"
+def uniqol_asset(path) = UniLib.path "UniQOL/UniQOLAssets/#{path}"
 
 #==========================================================================================================================================#
 #================================================================ OPTIONS =================================================================#
@@ -538,6 +532,14 @@ if ENABLE_MOVE_RELEARNER
 
   MOVE_RELEARN_FREE = UniStringOption.new("Free Relearning", "Party/PC relearn without costing a heart scale", %w[Off On])
 
+  if Reborn
+
+    UniLib.include "Map"
+
+    MapEvent.add_map_event(355) { |m| m.events[35].pages[0].list[10].instance_variable_set(:@parameters, [12, "$PokemonBag.pbQuantity(:HEARTSCALE)>0 || MOVE_RELEARN_FREE == 1"]) }
+
+  end
+
   MOVE_RELEARN_BEFORE_TUTOR = Reborn ? 1 : UniStringOption.new("Relearn Any Time", "Allows party relearning before unlocking the move relearner.", %w[Off On])
 
   def relearn_from_menu(pkmn)
@@ -550,8 +552,8 @@ if ENABLE_MOVE_RELEARNER
     end
   end
 
-  UniLib.add_party_command("move_relearner", "Relearn", proc { |pkmn| relearn_from_menu(pkmn) }, proc { ($game_switches[1444] || MOVE_RELEARN_BEFORE_TUTOR == 1) and MOVE_RELEARN_COMMAND >= 2 })
-  UniLib.add_box_command("move_relearner", "Relearn", proc { |pkmn| relearn_from_menu(pkmn) }, proc { ($game_switches[1444] || MOVE_RELEARN_BEFORE_TUTOR == 1) and MOVE_RELEARN_COMMAND & 1 == 1 })
+  UniLib.add_party_command("move_relearner", "Relearn", proc { |pkmn| relearn_from_menu(pkmn) }, proc { |pkmn| ($game_switches[1444] || MOVE_RELEARN_BEFORE_TUTOR == 1) and MOVE_RELEARN_COMMAND >= 2 and (!pkmn.relearner.is_a?(Array) or !pkmn.relearner[0])})
+  UniLib.add_box_command("move_relearner", "Relearn", proc { |pkmn| relearn_from_menu(pkmn) }, proc { ($game_switches[1444] || MOVE_RELEARN_BEFORE_TUTOR == 1) and MOVE_RELEARN_COMMAND & 1 == 1 and (!pkmn.relearner.is_a?(Array) or !pkmn.relearner[0])})
 
 end
 
