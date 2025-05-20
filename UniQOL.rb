44e0c2b9bf82dea2deea62a14a20c9a3d5389ccb
birblett <bwee.sylv@gmail.com>
2025-05-20 10:19:19 -0700

    free nature changing for reborn

--- UniQOL.rb
+++ UniQOL.rb
@@ -31,6 +31,7 @@ ENABLE_MAX_BAG_ITEM_OPTION = true
 ENABLE_HP_CHANGER = true
 ENABLE_ITEM_RADAR = true
 ENABLE_MOVE_RELEARNER = true
+ENABLE_FREE_NATURE_CHANGE = true
 ENABLE_MASS_RELEASE = true
 ENABLE_STORAGE_MODIFIER = true
 ENABLE_STAT_BOOST_DISPLAY = true
@@ -550,6 +551,28 @@ if ENABLE_MOVE_RELEARNER
 
   UniLib.add_party_command("move_relearner", "Relearn", proc { |pkmn| relearn_from_menu(pkmn) }, proc { |pkmn| ($game_switches[1444] || MOVE_RELEARN_BEFORE_TUTOR == 1) and MOVE_RELEARN_COMMAND >= 2 and (!pkmn.relearner.is_a?(Array) or !pkmn.relearner[0])})
   UniLib.add_box_command("move_relearner", "Relearn", proc { |pkmn| relearn_from_menu(pkmn) }, proc { ($game_switches[1444] || MOVE_RELEARN_BEFORE_TUTOR == 1) and MOVE_RELEARN_COMMAND & 1 == 1 and (!pkmn.relearner.is_a?(Array) or !pkmn.relearner[0])})
+
+end
+
+#============================================================== NATURE CHANGER ============================================================#
+
+if ENABLE_FREE_NATURE_CHANGE and Reborn
+
+  UniLib.include "Map"
+
+  NATURE_CHANGE_FREE = UniStringOption.new("Free Nature Change", "Pokemon psychologist without costing a heart scale", %w[Off On])
+
+  MapEvent.add_map_event(326) { |m|
+    m.events[3].pages[0].list[28].parameters[1] = "$PokemonBag.pbQuantity(:HEARTSCALE)>0 || NATURE_CHANGE_FREE == 1"
+    m.events[3].pages[0].list[45].parameters[1] = "$PokemonBag.pbDeleteItem(:HEARTSCALE) unless NATURE_CHANGE_FREE == 1"
+
+    m.events[3].pages[1].list[24].parameters[1] = "$PokemonBag.pbQuantity(:HEARTSCALE)>1 || NATURE_CHANGE_FREE == 1"
+    m.events[3].pages[1].list[150].parameters[1] = "$PokemonBag.pbDeleteItem(:HEARTSCALE) unless NATURE_CHANGE_FREE == 1"
+    m.events[3].pages[1].list[151].parameters[1] = "$PokemonBag.pbDeleteItem(:HEARTSCALE) unless NATURE_CHANGE_FREE == 1"
+
+    m.events[3].pages[2].list[25].parameters[1] = "$PokemonBag.pbQuantity(:HEARTSCALE)>1 || NATURE_CHANGE_FREE == 1"
+    m.events[3].pages[2].list[51].parameters[1] = "$PokemonBag.pbDeleteItem(:HEARTSCALE,2) unless NATURE_CHANGE_FREE == 1"
+  }
 
 end
 
